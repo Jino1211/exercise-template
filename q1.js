cc5756ac3a3f140ff2f4349dab7330d49df4aec7
Jino1211 <yone543@gmail.com>
2021-01-19 15:12:51 +0200

    q1

--- q1.js
+++ q1.js
@@ -2,6 +2,13 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
+
+    if (input[0] === '[' && input[input.length - 1] === ']') {
+        out = 'true';
+    } else {
+        out = 'false';
+    }
+    
 /**/
     // write your code here
     // out = input + 5;

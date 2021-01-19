156d0becbcf9207d041859019fa675deffde8e40
Jino1211 <yone543@gmail.com>
2021-01-19 20:18:22 +0200

    Update q24.js

--- q24.js
+++ q24.js
@@ -6,7 +6,7 @@ function run() {
     const arr = JSON.parse(input);   //because the JSON action is work only with 0,null. if you put something else he can't change it to an array
     let newArray = [];
     for (let i = 0; i < arr.length; i++) {
-        if ( arr[i]) {
+        if (arr[i]) {
             newArray.push(arr[i]);
         }
     }

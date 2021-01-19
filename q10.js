13961aba6814675907d0bee05d91e6a84eb72c6e
Jino1211 <yone543@gmail.com>
2021-01-19 17:28:20 +0200

    Update q10.js

--- q10.js
+++ q10.js
@@ -3,7 +3,8 @@ function run() {
     const output = document.getElementById("output");
     let out = "";
 
-    let arr = JSON.parse(input);
+    
+    const arr = JSON.parse(input);
     for (let i = 0; i < arr.length; i++) {
         console.log('row ' + i)
         for (let j = 0; j < arr.length-1; j++) {

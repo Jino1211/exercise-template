b0342b38cfa3b59c0d43185631b09d8376fdc2be
Jino1211 <yone543@gmail.com>
2021-01-19 20:06:34 +0200

    q24

---
+++ q24.js
@@ -0,0 +1,19 @@
+function run() {
+    const input = document.getElementById("input").value;
+    const output = document.getElementById("output");
+    let out = "";
+
+    const arr = JSON.parse(input);   //because the JSON action is work only with 0,null. if you put something else he can't change it to an array
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if ( arr[i]) {
+            newArray.push(arr[i]);
+        }
+    }
+    out = newArray;
+
+
+
+
+    output.innerText = out;
+}
